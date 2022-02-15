2e5c00e436eaf2596381c2662e53da4bf842af55
jair <jair.peixe@gmail.com>
2022-02-15 17:43:52 -0300

    Criação do arquivo lab7.pp, para gerenciamento do laboratório de eletrônica. Adicionado somente o chocolatey

---
+++ lab7.pp
@@ -0,0 +1,23 @@
+node
+    lab7-maq1,
+    lab7-maq2,
+    lab7-maq3,
+    lab7-maq4,
+    lab7-maq5,
+    lab7-maq6,
+    lab7-maq7,
+    lab7-maq8,
+    lab7-maq9,
+    lab7-maq10,
+    lab7-maq11,
+    lab7-maq12,
+    lab7-maq13,
+    lab7-maq14,
+    lab7-maq15,
+    lab7-maq16,
+    lab7-maq17,
+    lab7-maq18 {
+        
+        include chocolatey
+
+}
